311e20dee4eb525d7b1398d1fa0394c5ea411fdf
Dmytro Sihov <dima.sigov1@gmail.com>
2022-03-23 17:19:17 +0200

    task7 | removed asterisk

--- src/main/java/com/shop/util/Serializer.java
+++ src/main/java/com/shop/util/Serializer.java
@@ -6,7 +6,11 @@ import com.shop.entity.Product;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
