990d822e4f6a5b7d2af3abda816aa7dd840b2cdb
Dmytro Sihov <dima.sigov1@gmail.com>
2022-04-08 20:02:51 +0300

    task10 | installed login page template

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pre_prod_java_winter_2022
+https://colorlib.com/wp/template/login-form-v8/ - login page template
---
+++ src/main/webapp/WEB-INF/js/validation.js
@@ -0,0 +1,12 @@
+ $("#submit").click(function() {
+        let usernameField = document.getElementById("username");
+        console.log(usernameField.value);
+        let passwordField = document.getElementById("password");
+        console.log(passwordField.value);
+        let emailField = document.getElementById("email");
+        console.log(emailField.value);
+
+        let valid = true;
+
+        return valid;
+ });
